de0b6b331377af883652c9b8bea1d467d9aa9eff
jordan-rexroad <111549058+jordan-rexroad@users.noreply.github.com>
2023-03-19 00:21:59 -0500

    FeaturesList and state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
 import './App.css';
+import FeaturesForm from './Components/FeaturesForm/FeaturesForm';
+import FeaturesList from './Comonents/FeaturesList/FeaturesList'
 
-function App() {
-  return (
+function App() { 
+  const [featureItems, setFeatureItems] = React.useState([
+    {
+      feature: 'Class syntax',
+      version: 'ES6',
+      year: 2015
+    },
+    {
+      feature:'Object destructuring',
+      version: 'ES9',
+      year: 2018
+    }
+  ]);
+
+    return (
     <div className="App">
-      Empty
+      <h1>JavaScript Features App</h1>
+      <FeaturesForm />
+      <FeaturesList />
+      <div>List</div>
     </div>
   );
 }
